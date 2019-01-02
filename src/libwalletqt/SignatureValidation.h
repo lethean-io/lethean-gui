e21bd6fc198a85969af52d6fc1d9bef72349d322
Laion Camargo <laion.camargo@alfasoft.pt>
2019-01-02 13:02:48 +0100

    add signature validation

--- src/libwalletqt/SignatureValidation.h
+++ src/libwalletqt/SignatureValidation.h
@@ -3,24 +3,17 @@
 
 #include <QObject>
 #include <QDebug>
-#include <stdio.h>
 #include <QString>
-#include <iostream>
 #include <string>
-#include <algorithm>
-
-#include <vector>
-#include <QByteArray>
-#include <QBitArray>
 
 #include "../ed25519-donna/ed25519.h"
 
+// validate signature from SDP - json
 class SignatureValidation : public QObject
 {
     Q_OBJECT
 public:
-    Q_INVOKABLE bool verify(QString message, QString signature, QString pulicKey);
-    Q_INVOKABLE QString base64_encode(QString message);
+    Q_INVOKABLE bool verify(QString message, QString signature, QString pulicKey, int msgLength);
 
 };
 
